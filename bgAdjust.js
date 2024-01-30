b176d76ad7ffd9c5fbeb3f839de4e23cd86812d1
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-01-30 17:31:12 +0530

    changes

--- bgAdjust.js
+++ bgAdjust.js
@@ -1,44 +1,8 @@
-const preloader = document.querySelector(".preloader");
-let domain = document.querySelector("#domain");
-let mask=document.querySelector('#mask')
-let section1=document.querySelector('.section1')
-let section2=document.querySelector('.section2')
+let preloader= document.querySelector('.preloader')
 window.addEventListener("load", () => {
   preloader.classList.add("hide-preloader");
 
 });
-// backgroundImgAdjust(domain,section1)
-// backgroundImgAdjust(mask,section2)
 
-function backgroundImgAdjust(img,section){
 
-  let imgHeight = img.height;
-  let imgWidth = img.width;
-  let hratio = window.innerHeight / imgHeight;
-  let wratio = window.innerWidth / imgWidth;
-  let ratio = Math.max(hratio, wratio);
-  let centerY = Math.floor(window.innerHeight - imgHeight * ratio) / 2;
-  let centerX = Math.floor(window.innerWidth - imgWidth * ratio) / 2;
   
-  
-  window.addEventListener("load", () => {
-      preloader.classList.add("hide-preloader");
-  
-      resize();
-  });
-  function resize() {
-    hratio = window.innerHeight / imgHeight;
-    wratio = window.innerWidth / imgWidth;
-    ratio = Math.max(hratio, wratio);
-    centerY = Math.floor(window.innerHeight - imgHeight * ratio) / 2;
-    centerX = Math.floor(window.innerWidth - imgWidth * ratio) / 2;
-    img.height = ratio * imgHeight;
-    section.height=img.height
-    img.width = ratio * imgWidth;
-  
-    img.style.transform=`translateX(-${centerX}px)`
-  }
-  window.addEventListener("resize", ()=>{
-      resize()
-  });
-}
